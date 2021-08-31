7d218ed4d1deeabfffeb4820328cc1c7db5973ee
Jeff <zl1571098645@163.com>
2021-08-31 23:06:41 +0800

    SRP初探、URP源码

--- Assets/Pipline/MyCustomSRPAsset.cs
+++ Assets/Pipline/MyCustomSRPAsset.cs
@@ -11,40 +11,42 @@ using Conditional = System.Diagnostics.ConditionalAttribute;
 [CreateAssetMenu(menuName = "Rendering/我的自定义SRP")]
 public class MyCustomSRPAsset : RenderPipelineAsset
 {
-    protected override IRenderPipeline InternalCreatePipeline()
+    protected override RenderPipeline CreatePipeline()
     {
-        return new CustomSRP();
+        return new CustomRP();
     }
 }
 
 //自定义管线类，继承RenderPipeline，主要实现Render方法
-public class CustomSRP: RenderPipeline
+public class CustomRP: RenderPipeline
 {
-    private CommandBuffer commandBuffer;
+
     private ScriptableCullingParameters cullingParameters;
-    private CullResults cullResults;
+    private CullingResults cullResults;
 
-    public CustomSRP()
+    CameraRenderer renderer = new CameraRenderer();
+
+    public CustomRP()
     {
-        commandBuffer = new CommandBuffer { name = "Render Cam" };
-        cullResults = new CullResults();
+        cullResults = new CullingResults();
     }
 
-    public override void Dispose()
-    {
-        base.Dispose();
-    }
+   //public override void Dispose(bool disposing)
+   //{
+   //    base.Dispose();
+   //}
 
     //遍历执行Camera的Render方法
-    public override void Render(ScriptableRenderContext renderContext, Camera[] cameras)
+    protected override void Render(ScriptableRenderContext renderContext, Camera[] cameras)
     {
-        base.Render(renderContext, cameras);
+        //base.Render(renderContext, cameras);
         foreach (var cam in cameras)
         {
-            Render(renderContext, cam);
+            renderer.Render(renderContext, cam);       
         }
     }
 
+    /*
     //单个Camera的Render方法
     private void Render(ScriptableRenderContext renderContext, Camera camera)
     {
@@ -61,7 +63,7 @@ public class CustomSRP: RenderPipeline
             camera.backgroundColor );
 
         //剔除
-        if(!CullResults.GetCullingParameters(camera, out cullingParameters))
+        if(!CullingResults.GetCullingParameters(camera, out cullingParameters))
         {
             return;
         }
@@ -69,7 +71,7 @@ public class CustomSRP: RenderPipeline
         if(camera.cameraType == CameraType.SceneView)
             ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
 #endif
-        CullResults.Cull(ref cullingParameters, renderContext, ref cullResults);
+        CullingResults.Cull(ref cullingParameters, renderContext, ref cullResults);
 
 
         //绘制
@@ -98,6 +100,7 @@ public class CustomSRP: RenderPipeline
 
         renderContext.Submit();
     }
+    
 
     Material error;
     [Conditional("DEVELOPMENT_BUILD"), Conditional("UNITY_EDITOR")]
@@ -120,5 +123,94 @@ public class CustomSRP: RenderPipeline
         drawSettings.SetOverrideMaterial(error, 0);
         var filterSettings = new FilterRenderersSettings(true);
         context.DrawRenderers(cullResults.visibleRenderers, ref drawSettings, filterSettings);
+    }
+    */
+}
+
+public class CameraRenderer
+{
+    ScriptableRenderContext context;
+
+    Camera camera;
+
+    static ShaderTagId unlitShaderTagID = new ShaderTagId("SRPDefaultUnlit");
+
+    const string bufferName = "---Render Camera---";
+    private CommandBuffer commandBuffer = new CommandBuffer { name = bufferName };
+
+    public void Render(ScriptableRenderContext ctx, Camera cam)
+    {
+        context = ctx;
+        camera = cam;
+
+        //剔除检测
+        if (!Cull()) 
+        {
+            return;
+        }
+
+        Setup();
+
+        DrawVisableGeometry();
+
+        Submit();
+    }
+
+    void Setup()
+    {
+        context.SetupCameraProperties(camera);
+
+        //commandBuffer里注入样本，可以在Profiler和FrameDebugger里看到，需要有开始和结束
+        commandBuffer.BeginSample(bufferName);
+
+        //渲染前ClearRT 清除Depth、Color、Stencil三个buffer
+        commandBuffer.ClearRenderTarget(true, true, Color.clear); 
+
+        ExecuteBuffer();
+
+    }
+
+    /// <summary>
+    /// 画几何体
+    /// </summary>
+    void DrawVisableGeometry()
+    {
+        //排序设置、绘制设置 、过滤设置
+        SortingSettings sortingSettings = new SortingSettings(camera) {
+            criteria = SortingCriteria.CommonOpaque
+        };
+        DrawingSettings drawingSettings = new DrawingSettings(unlitShaderTagID, sortingSettings);
+        FilteringSettings filteringSettings = new FilteringSettings(RenderQueueRange.all);
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+
+        //天空盒
+        context.DrawSkybox(camera);
+    }
+
+    void Submit()
+    {
+        commandBuffer.EndSample(bufferName);
+
+        ExecuteBuffer();
+
+        context.Submit();
+    }
+
+    void ExecuteBuffer()
+    {
+        //执行和清除buffer通常在一起
+        context.ExecuteCommandBuffer(commandBuffer);
+        commandBuffer.Clear();
+    }
+
+    CullingResults cullingResults;
+    bool Cull()
+    {
+        if(camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
     }
 }
